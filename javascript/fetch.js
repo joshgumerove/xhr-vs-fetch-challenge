a8d47bb4cee2e599e0a934d8189508160aa7d5c2
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-01-23 20:26:55 -0500

    fetch response

---
+++ javascript/fetch.js
@@ -0,0 +1,15 @@
+const btn2 = document.getElementById("two");
+const outputTwo = document.getElementById("output");
+btn2.addEventListener("click", fetchClickHandler);
+
+function fetchClickHandler() {
+  fetch("https://icanhazdadjoke.com/", {
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then(({ joke }) => {
+      outputTwo.innerHTML = joke;
+    });
+}
